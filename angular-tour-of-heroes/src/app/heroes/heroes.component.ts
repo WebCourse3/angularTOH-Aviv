c3467eb8689a9b6a7ef90e4cd08b702616c47608
Aviv454 <Aviv454@gmail.com>
2017-11-12 16:53:17 +0200

    amgular TOH

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+}
